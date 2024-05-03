20450fa4b39b08aa1455e0307e5f1a98c2b428f0
Amrinder Singh <amrinder995@gmail.com>
2024-05-03 01:40:44 -0400

    implement new language link, edit language link, and delete language link

--- app/controllers/programming_languages_controller.rb
+++ app/controllers/programming_languages_controller.rb
@@ -1,4 +1,5 @@
 class ProgrammingLanguagesController < ApplicationController
+
   def index
     @programming_languages = ProgrammingLanguage.all
   end
@@ -8,17 +9,41 @@ class ProgrammingLanguagesController < ApplicationController
   end
 
   def new
+    @language = ProgrammingLanguage.new
   end
 
   def create
+    @language = ProgrammingLanguage.new(programming_language_params)
+    if @language.save
+      redirect_to programming_language_path(@language), notice: 'Programming language was successfully added.'
+    else
+      render :new
+    end
   end
 
   def edit
+    @language = ProgrammingLanguage.find(params[:id])
   end
 
   def update
+    @language = ProgrammingLanguage.find(params[:id])
+    @language.update(programming_language_params)
+    if @language.save
+      redirect_to programming_language_path(@language), notice: 'Programming language was successfully edited.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @language = ProgrammingLanguage.find(params[:id])
+    @language.destroy
   end
+
+  private 
+
+  def programming_language_params
+    params.require(:programming_language).permit(:title, :description, :link)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,21 @@ Rails.application.routes.draw do
   
   get 'programming_languages/index'
   get 'programming_languages/show'
-  get 'programming_languages/new'
+
+  get 'programming_languages/new',
+  to: 'programming_languages#new',
+  as: 'new_programming_language_path'
+
   get 'programming_languages/create'
-  get 'programming_languages/edit'
+
+  get 'programming_languages/edit',
+  to: 'programming_languages#edit',
+  as: 'edit_programming_language_path'
+
   get 'programming_languages/update'
-  get 'programming_languages/destroy'
+  
+  get 'programming_languages/destroy',
+  to: 'programming_languages#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
